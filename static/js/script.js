c4f82dad9ff1150453515f6eb60285109d40eb58
Deeply04 <bakhutashvili19@gmail.com>
2022-08-05 18:35:43 +0400

    Homework-8

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+let myObject = {
+    name: 'davit',
+    lastName: 'Bakhutashvili',
+    age: 22,
+    Emain: 'davit.bakhutashvili.1@iliauni.edu.ge',
+    City: 'Tbilisi'
+}
+
+console.log(myObject)
